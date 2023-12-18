1ca0c279cb5f758cf0df6a0d8c73ddd7ae81fb89
Konstantinos Daravagkas <k.daravagkas@corenetworks.gr>
2023-12-18 16:29:31 +0200

    'first'

---
+++ learning_templetes/basic_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context_dix = {'text':'hello world','number':100}
+    return render(request,'basic_app/index.html',context_dix)
+
+def other(request):
+    return render(request,'basic_app/other.html')
+def relative(request):
+    return render(request,'basic_app/relative.html')
